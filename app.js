ca3a35c4d86902dd992361497714623b55c95e56
Thiago Ueda <thiagokoiti@gmail.com>
2025-01-21 16:49:54 -0300

    Adiciona a lista listaAmigos, as funções adicionarAmigo e limparCampo no arquivo app.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaAmigos = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    listaAmigos.push(nome);
+    console.log(listaAmigos);
+    limparCampo();
+    return listaAmigos;
+    
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
