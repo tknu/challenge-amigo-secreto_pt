e8ab84226b435c24bd61c19418f884eec8648835
Thiago Ueda <thiagokoiti@gmail.com>
2025-01-21 20:35:00 -0300

    Adiciona controle se não digitar um nome e criação das funções mostrarLista e adicionarNomeamigo

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
+let nome;
 
 function adicionarAmigo() {
-    let nome = document.querySelector('input').value;
+    nome = document.querySelector('input').value;
+    if (nome == '') {
+        alertInserirNome();
+    } else {
+        adicionarNomeAmigo();
+        
+    }  
+}
+
+function adicionarNomeAmigo() {
     listaAmigos.push(nome);
     console.log(listaAmigos);
     limparCampo();
     return listaAmigos;
+}
+
+function mostrarLista() {
     
 }
 
 function limparCampo() {
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function alertInserirNome () {
+    inserirNome = alert('Por favor, insira um nome.');
+    return inserirNome;
 }
