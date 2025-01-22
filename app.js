316f6d112b69bc400789d04a41354067d96e53cf
Thiago Ueda <thiagokoiti@gmail.com>
2025-01-22 16:50:53 -0300

    adiciona inserção de nome na lista do input pela tecla enter, implementa as funções sortearAmigo, mostrarLista, alertInserirNome

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
 let nome;
+let amigoSecreto ='';
 
 function adicionarAmigo() {
     nome = document.querySelector('input').value;
@@ -16,11 +17,33 @@ function adicionarNomeAmigo() {
     listaAmigos.push(nome);
     console.log(listaAmigos);
     limparCampo();
+    mostrarLista();
     return listaAmigos;
 }
 
+document.querySelector('input').addEventListener('keypress', function(event) {
+    if (event.key === 'Enter') {
+        adicionarAmigo(); 
+    }
+});
+
+function sortearAmigo() {
+    let indiceAmigoSecreto = parseInt(Math.random() * listaAmigos.length)
+    amigoSecreto = listaAmigos[indiceAmigoSecreto];
+    return console.log(amigoSecreto);
+}
+
 function mostrarLista() {
-    
+    /*
+    document.getElementById('listaAmigos').innerHTML = 
+    listaAmigos.map(item=>`<li>${item}</li>`).join('');
+    */
+   let listaHtml = '';
+   for (let i = 0; i < listaAmigos.length; i++) {
+        listaHtml += `<li>${listaAmigos[i]}</li>`;
+   }
+   document.getElementById('listaAmigos').innerHTML = listaHtml;
+   
 }
 
 function limparCampo() {
